9ccce22523db4c66db1be186ad4cb2d264a0d274
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2023-07-26 01:54:13 +0200

    Allow for ?w=full in image imports

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 // cSpell:words imagetools
 
 import { sveltekit } from '@sveltejs/kit/vite'
-import { imagetools } from 'vite-imagetools'
+import { imagetools, resolveConfigs } from 'vite-imagetools'
 import doSync from 'do-sync'
 import path from 'path'
 
 /** @type {import('imagetools-core').OutputFormat} */
 const imageFormat = () => (metadatas) => {
+  metadatas.sort((a, b) => b.width - a.width)
+
   /** @type {{[format: string]: typeof metadatas[number]}} */
   const formats = {}
   metadatas.forEach((meta) => {
@@ -24,12 +26,16 @@ const imageFormat = () => (metadatas) => {
 
   /** @type {string} */
   const file = metadatas[0].image.options.input.file
-  /** @type {number} */
-  const aspect = metadatas[0].aspect
   const data = doSync.doSync(async (file) => {
     const sharp = global['require']('sharp')
     return (await sharp(file).resize({ width: 20 }).png().toBuffer()).toString('base64')
   })(file)
+
+  /** @type {number | undefined} */
+  const metaAspect = metadatas.map((meta) => meta.aspect).find((aspect) => typeof aspect === 'number')
+  /** @type {number} */
+  const sizeAspect = metadatas[0].width / metadatas[0].height
+  const aspect = typeof metaAspect === 'undefined' ? sizeAspect : metaAspect
 
   /** @type {import('./src/lib/types').Image} */
   return {
@@ -46,6 +52,17 @@ const config = {
     sveltekit(),
     imagetools({
       extendOutputFormats: (defaults) => ({ ...defaults, Image: imageFormat }),
+      resolveConfigs: (entries, outputFormats) => {
+        const out = resolveConfigs(entries, outputFormats)
+        return out.map((directives) => {
+          if ('Image' in directives) {
+            return Object.fromEntries(
+              Object.entries(directives).filter(([key, values]) => key !== 'w' || values !== 'full')
+            )
+          }
+          return directives
+        })
+      },
     }),
   ],
   assetsInclude: ['**/*.xml'],
