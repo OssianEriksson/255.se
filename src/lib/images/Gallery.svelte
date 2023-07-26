a0d7c641bcf5c93ed8453571663e693e29f7865f
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2023-07-26 12:56:06 +0200

    Fade out gallery controls

--- src/lib/images/Gallery.svelte
+++ src/lib/images/Gallery.svelte
@@ -10,17 +10,17 @@
 
 <script lang="ts">
   import type { Src } from '.'
-  import { setContext, createEventDispatcher } from 'svelte'
+  import { setContext, createEventDispatcher, onMount } from 'svelte'
   import { fade } from 'svelte/transition'
   import { Image, GalleryGrid, galleryKey } from '.'
   import MdClose from 'svelte-icons/md/MdClose.svelte'
   import MdChevronLeft from 'svelte-icons/md/MdChevronLeft.svelte'
   import MdChevronRight from 'svelte-icons/md/MdChevronRight.svelte'
 
-  let sources: Src[] = []
-
   export let index: number | undefined = undefined
   export let noGrid = false
+
+  let sources: Src[] = []
 
   setContext(galleryKey, {
     registerImage: (src) => {
@@ -35,8 +35,52 @@
 
   function select(idx: number | undefined) {
     index = idx
+    pushBackUIFadeOut()
     dispatch('select', idx)
   }
+
+  let uiVisible = true
+  let uiFade = false
+
+  let uiFadeOutTimer: ReturnType<typeof setTimeout> | null = null
+
+  function clearUIFadeOutTimer() {
+    if (uiFadeOutTimer) {
+      clearTimeout(uiFadeOutTimer)
+      uiFadeOutTimer = null
+    }
+  }
+
+  function pushBackUIFadeOut() {
+    uiVisible = true
+    uiFade = false
+    clearUIFadeOutTimer()
+    uiFadeOutTimer = setTimeout(() => {
+      uiVisible = false
+      uiFade = true
+    }, 3000)
+  }
+
+  function toggleUIVisible() {
+    uiVisible = !uiVisible
+    uiFade = false
+    if (uiVisible) {
+      pushBackUIFadeOut()
+    } else {
+      clearUIFadeOutTimer()
+    }
+  }
+
+  let lastMouseMoveTime = 0
+  function onMouseMove() {
+    const currentTime = Date.now()
+    if (currentTime - lastMouseMoveTime < 200) {
+      pushBackUIFadeOut()
+    }
+    lastMouseMoveTime = currentTime
+  }
+
+  onMount(pushBackUIFadeOut)
 
   const transDuration = Number(tailwindcss.config.theme.transitionDuration?.DEFAULT?.slice(0, -2)) || 0
 </script>
@@ -49,7 +93,13 @@
   </GalleryGrid>
 {/if}
 {#if typeof index !== 'undefined' && index >= 0 && index < sources.length}
-  <div transition:fade={{ duration: transDuration }} class="fixed h-full w-full bg-white top-0 left-0">
+  <div
+    transition:fade={{ duration: transDuration }}
+    class="fixed h-full w-full bg-white top-0 left-0"
+    on:click={toggleUIVisible}
+    on:keypress={toggleUIVisible}
+    on:mousemove={onMouseMove}
+  >
     <Image
       class="max-w-full max-h-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       src={sources[index]}
@@ -57,22 +107,31 @@
       gallery={false}
     />
     <button
-      class="text-white bg-black absolute right-0 top-0 m-4 p-2 w-16 h-16 flex justify-center items-center rounded-full bg-opacity-30 hover:bg-opacity-80 text-opacity-60 hover:text-opacity-100 transition-colors"
-      on:click={() => select(undefined)}
+      class={`text-white bg-black absolute right-0 top-0 m-4 p-2 w-16 h-16 flex justify-center items-center rounded-full bg-opacity-30 hover:bg-opacity-80 text-opacity-60 hover:text-opacity-100 ${
+        uiVisible ? 'opacity-100' : 'opacity-0'
+      } duration-150 ease-in-out`}
+      style={`transition: color 0.15s, background-color 0.15s, opacity ${uiFade ? '2s' : '0.15s'}`}
+      on:click|stopPropagation={() => select(undefined)}
     >
       <MdClose />
     </button>
     <button
       disabled={index <= 0}
-      class="text-white bg-black absolute left-0 top-1/2 -translate-y-1/2 w-16 h-24 ml-4 flex justify-center items-center rounded-lg bg-opacity-30 hover:bg-opacity-80 text-opacity-60 hover:text-opacity-100 disabled:opacity-0 transition-[colors,opacity]"
-      on:click={() => select(typeof index === 'undefined' ? undefined : index - 1)}
+      class={`text-white bg-black absolute left-0 top-1/2 -translate-y-1/2 w-16 h-24 ml-4 flex justify-center items-center rounded-lg bg-opacity-30 hover:bg-opacity-80 text-opacity-60 hover:text-opacity-100 disabled:opacity-0 ${
+        uiVisible ? 'opacity-100' : 'opacity-0'
+      } duration-150 ease-in-out`}
+      style={`transition: color 0.15s, background-color 0.15s, opacity ${uiFade ? '2s' : '0.15s'}`}
+      on:click|stopPropagation={() => select(typeof index === 'undefined' ? undefined : index - 1)}
     >
       <MdChevronLeft />
     </button>
     <button
       disabled={index >= sources.length - 1}
-      class="text-white bg-black absolute right-0 top-1/2 -translate-y-1/2 w-16 h-24 mr-4 flex justify-center items-center rounded-lg bg-opacity-30 hover:bg-opacity-80 text-opacity-60 hover:text-opacity-100 disabled:opacity-0 transition-[colors,opacity]"
-      on:click={() => select(typeof index === 'undefined' ? undefined : index + 1)}
+      class={`text-white bg-black absolute right-0 top-1/2 -translate-y-1/2 w-16 h-24 mr-4 flex justify-center items-center rounded-lg bg-opacity-30 hover:bg-opacity-80 text-opacity-60 hover:text-opacity-100 disabled:opacity-0 ${
+        uiVisible ? 'opacity-100' : 'opacity-0'
+      } duration-150 ease-in-out`}
+      style={`transition: color 0.15s, background-color 0.15s, opacity ${uiFade ? '2s' : '0.15s'}`}
+      on:click|stopPropagation={() => select(typeof index === 'undefined' ? undefined : index + 1)}
     >
       <MdChevronRight />
     </button>
