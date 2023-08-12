8566c87609afe976e699066c5e294b1c8618fd68
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2023-08-12 16:55:04 +0200

    Edit on github links directly to current route

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { ObfuscatedAnchor, MAILTO_HELLO_DANCIS_SE } from '$lib/obfuscated-anchor'
+  import { page } from '$app/stores'
 </script>
 
 <div class="bg-gray-200 flex flex-col items-center">
   <span class="m-4">
-    <a class="hover:underline" href="https://github.com/OssianEriksson/255.se/tree/main/src/routes">
+    <a class="hover:underline" href={`https://github.com/OssianEriksson/255.se/tree/main/src/routes${$page.route.id}`}>
       Redigera på GitHub
     </a>
     | <ObfuscatedAnchor class="hover:underline" href={MAILTO_HELLO_DANCIS_SE}>Webbansvarig</ObfuscatedAnchor>
