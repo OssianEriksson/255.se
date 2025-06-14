28b4423ca0f38be6e6419eff35ed10ea364bcf26
Dancis <hello@dancis.se>
2025-06-14 16:51:22 +0000

    chore: remove table of contents indent

--- src/lib/TableOfContents.svelte
+++ src/lib/TableOfContents.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <h2>Innehåll</h2>
-<div class={`[&_li]:list-none [&_ul]:!pl-4${sections ? ' [&>ul>li]:!mt-4 [&>ul>li>a]:font-bold' : ''}`}>
+<div class={`[&_li]:list-none [&_ul]:!pl-0 [&_ul_ul]:!pl-4${sections ? ' [&>ul>li]:!mt-4 [&>ul>li>a]:font-bold' : ''}`}>
   <slot />
 </div>
